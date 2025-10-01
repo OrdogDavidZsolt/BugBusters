39a175da840d3fbd844327a54ecc5bc74b269b57
TBence04 <t.bence.unideb@gmail.com>
2025-10-01 15:40:36 +0200

    Külön mappa és üres kód a kártyaolvasó szoftverének

---
+++ Hardwer_Code/cardreader/cardreader.ino
@@ -0,0 +1,18 @@
+/* This is the code for the ESP32-C3 based card reader
+ *
+ * MCU Type         : ESP32-C3 (single core, RISC-V)
+ * RFID reader      : MFRC-522 (SPI)
+ * Ethernet module  : W5500 based (SPI)
+ */
+
+
+ 
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
