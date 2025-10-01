9c6293c7bace19efee004a1b8da499605e28f12c
TBence04 <t.bence.unideb@gmail.com>
2025-10-01 21:36:17 +0200

    On branch main  Changes to be committed: 	modified:   Software_Code/Backend_Server/Controller.java
    processID metódus inicializálása, teszt kiíratás

--- Software_Code/Backend_Server/Controller.java
+++ Software_Code/Backend_Server/Controller.java
@@ -10,4 +10,22 @@ public class Controller {
     public static void main(String[] args) {
         
     }
+
+    public static void processID(String CardID)
+    {
+        /* Ez a metódus feldolgozza a kártyaolvasótól érkező adatokat
+         *
+         * Paraméter: String CardID -> a kártyaolvasó által küldött kártyaazonosító 
+         * Meghívja: HW_Connection.java osztály
+         * Feladata: Az adatbázisból kideríti lekérdezés(ek)-el, hogy tanár vagy
+         *           hallgató ID-t jelent a CardID paraméter.
+         *           Ezalapján vagy elindítja a timert (20p) és új konténert hoz
+         *           létre a további kártyák tárolására
+         *           vagy pedig (ha hallgatóról van szó) hozzáadja az adatokat a
+         *           megfelelő tárolóhoz.
+         */
+
+        // teszt jellegű kiíratás
+        System.out.println(CardID);
+    }
 }
