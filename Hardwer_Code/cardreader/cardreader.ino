f71ae35392b35df299a3178c963434b614b200fc
TBence04 <t.bence.unideb@gmail.com>
2025-10-01 21:58:46 +0200

    On branch main  Changes to be committed: 	modified:   Hardwer_Code/cardreader/cardreader.ino
    Setup eljárás elkészítése a kártyaolvasó szoftverében

--- Hardwer_Code/cardreader/cardreader.ino
+++ Hardwer_Code/cardreader/cardreader.ino
@@ -5,11 +5,46 @@
  * Ethernet module  : W5500 based (SPI)
  */
 
+// ------------- Includes -------------
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <ESPmDNS.h>
 
+
+// ------------ Globals -------------
+byte mac[6]; // MAC address of W5500, walue from ESP's MAC
  
 void setup()
 {
+    Serial.begin(115200);
 
+    uint64_t chipMac = ESP.getEfuseMac(); // ESP's factory MAC, 48 bits
+    mac[0] = (chipMac >> 40) & 0xFF;
+    mac[1] = (chipMac >> 32) & 0xFF;
+    mac[2] = (chipMac >> 24) & 0xFF;
+    mac[3] = (chipMac >> 16) & 0xFF;
+    mac[4] = (chipMac >> 8 ) & 0xFF;
+    mac[5] =  chipMac & 0xFF;
+
+    // DEBUG {
+    Serial.print("ESP mac: ");
+    for (int i = 0; i < 6; i++)
+    {
+        Serial.print(mac[i], HEX);
+    }
+    Serial.println();
+    // DEBUG }
+
+    // Initialize W5500 via DHCP
+    while (Ethernet.begin(mac) == 0)
+    {
+        Serial.println("DHCP failed"); // Error msg
+        delay(1000);    // Try again after 1 sec
+    }
+    Serial.println("DHCP configured:");  // Debug msg
+    Serial.print("Assigned IP: "); Serial.println(Ethernet.localIP()); // IP address from DHCP
+    
 }
 
 void loop()
