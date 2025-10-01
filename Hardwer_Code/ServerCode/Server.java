4f2a5f145db9d0cf10a3439379284238613a9288
Száva Gréta <szvgrt2004@mailbox.unideb.hu>
2025-10-01 16:20:35 +0200

    konstansok létrehozása, ezek példa értékek, majd az elektronika megépítése után frissítem

--- Hardwer_Code/ServerCode/Server.java
+++ Hardwer_Code/ServerCode/Server.java
@@ -1,9 +1,11 @@
 package ServerCode;
 
 import java.io.*; //hálózatról való olvasáshoz kell
-
+import java.net.*; //hálózati kapcsolatokat kezel
+import java.util.concurrent.*; //a több szálon futáshoz
 
 public class Server
 {
-
+    private static final int PORT = 54321;
+    private static final int THREAD_POOL_SIZE = 50; // egyszerre max. 50 kliens
 }
