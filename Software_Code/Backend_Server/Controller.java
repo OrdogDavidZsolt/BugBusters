bb78fafde87b735754470ad4a74cb6b25d3b2c41
TBence04 <t.bence.unideb@gmail.com>
2025-10-01 16:53:34 +0200

    On branch main  Changes to be committed: 	modified:   Software_Code/Backend_Server/Controller.java
    Ebben a commitban inicializáltam a Software_Code/Backend_Server/Controller.java kódot
    Csak ebben legyen main metódus, a többi komponensbe nem kell!

---
+++ Software_Code/Backend_Server/Controller.java
@@ -0,0 +1,13 @@
+/**
+ * Ez a fájl indítja el a teljes backend szervert
+ * A többi kódban csak metódusok (API-k) legyenek, main nem kell!
+ */
+
+package Software_Code.Backend_Server;
+
+public class Controller {
+    
+    public static void main(String[] args) {
+        
+    }
+}
