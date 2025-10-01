70fdf36646a5d50f3fcc75f702ec7c26ffbec016
TBence04 <t.bence.unideb@gmail.com>
2025-10-01 15:17:30 +0200

    On branch dev  Changes to be committed: 	modified:   Proba/Main2.java Egy újabb kiíratás, commit a dev branch-ben

--- Proba/Main2.java
+++ Proba/Main2.java
@@ -2,5 +2,6 @@ class Main2
 {
     public static void main(String[] args) {
         System.out.println("Hello from Bence");
+        System.out.println("Ez a második commit a dev branchben");
     }
 }
