71c37357f7cb9248862122adfbc42b2512c6f5d1
TBence04 <t.bence.unideb@gmail.com>
2025-10-04 18:24:54 +0200

    On branch main  Changes to be committed: 	modified:   Software_Code/Backend_Server/UI_Connection/UI_Connection.java
    Importok elkészítése a UI szervernél

--- Software_Code/Backend_Server/UI_Connection/UI_Connection.java
+++ Software_Code/Backend_Server/UI_Connection/UI_Connection.java
@@ -1,4 +1,21 @@
+/**
+ * Ebben az osztályban egy web kiszolgálót implementálok, mely a UI kapcsolatért felel
+ * 
+ * Az osztályban egy 80-as porton működő HTTP szerver van, amely adatokat küld és fogad,
+ * illetve a HTML oldalak betöltéséért felel
+ */
+
 package Software_Code.Backend_Server.UI_Connection;
+
+// ------------- Importok -------------
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 
 public class UI_Connection {
     
