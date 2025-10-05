325b0a5b5f60b9bbbbd669f3a951e690807558c4
TBence04 <t.bence.unideb@gmail.com>
2025-10-05 16:54:24 +0200

    On branch dev  Changes to be committed: 	modified:   Software_Code/Backend_Server/Controller/Controller.java 	modified:   Software_Code/Backend_Server/UI_Connection/UI_Connection.java
    UI szerver indító metódus áthelyezése: Controller.java -> UI_Conneciton.java

--- Software_Code/Backend_Server/Controller/Controller.java
+++ Software_Code/Backend_Server/Controller/Controller.java
@@ -15,59 +15,17 @@ package Software_Code.Backend_Server.Controller;
 
 // ------------- Importok -------------
 // UI_Conncetion szükségletek
-import com.sun.net.httpserver.HttpServer;
 import Software_Code.Backend_Server.UI_Connection.UI_Connection;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+
 
 
 
 public class Controller {
     
     public static void main(String[] args) {
-        start_UI_Server();  // UI szerver elindítása
+        UI_Connection.start_UI_Server();  // UI szerver elindítása
     }
-
-
-    public static void start_UI_Server()
-    {
-        try
-        {
-            /**
-             * Létrehozzuk a HTTP szerver objektumot
-             * InetSocketAddress: Megmondja, hogy a futtató gép ip címén létezzen a szerver, illetve, hogy melyik porton
-             * Második paraméter: backlog -> a várakozó kapcsolatok száma
-             *      Mivel backlog == 0, a váarkozó kapcsolatok számát az oprendszerfogja korlátozni max. 128-ra (!)
-             * Ez a  .create() metódus még nem indítja el a szervert, csak a kódobjektumot hozza létre
-             */
-            HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
-            /**
-             * createContext: endpoint hozzáadása a szerverhez
-             *      második paraméter a HttpHandler-t implementáló osztály: UI_Connection.java
-             *      ez az osztály tartalmaz egy 'public void handle(HttpExchange exchange)' metódust
-             */
-            server.createContext("/", new UI_Connection());
-            /**
-             * setExecutor: a HttpServer külön szálakon képes kéréseket kezelni
-             * az executor határozza meg a szálakat
-             * null -> egy beépített automatikus szálkezelő logika, nem kell 'bántani'
-             * máshogy: nem konfigurálok saját thread poolt a szerverhez, azautomaikusat használom
-             */
-            server.setExecutor(null);
-            /**
-             * a server.start() indítja el valójában a szervert és megnyitja a 80-as portot
-             * innentől a szerver figyeli a 80-as portra érkező HTTP kéréseket
-             * minden kéréshez meghívja a megfelelő handlert
-             */
-            server.start();
-
-            System.out.println(">>Controller: UI_Server started on port 80!");
-        }
-        catch (IOException e)
-        {
-            System.out.println(">>Controller: IO exception upon UI_Server start: " + e.getMessage());
-        }
-    }
+    
 }
 
