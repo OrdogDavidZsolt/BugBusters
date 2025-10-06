3ce16985a3e3819736e68d9816c08c8dedf67600
greti <szvgrt2004@mailbox.unideb.hu>
2025-10-06 16:47:34 +0200

    a szalkezeleshez szukseges eszkozok megertesere egy probaprojekt jott letre

---
+++ Software_Code/Backend_Server/HW_Connection/szalkezelesProba.java
@@ -0,0 +1,18 @@
+package Software_Code.Backend_Server.HW_Connection;
+
+public class szalkezelesProba
+{
+    public static void main(String[] args)
+    {
+        Runnable t = () -> {
+           String s = Thread.currentThread().getName();
+            System.out.println("Fut a szál: " + s);
+        };
+
+        //inditunk ket kulon szalat
+        new Thread(t, "Elso szal").start();
+        new Thread(t, "Masodik szal").start();
+
+        System.out.println("Fut a main szal");
+    }
+}
