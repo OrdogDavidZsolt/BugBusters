f75e7a81a4beef2d154610c7167d5fbc284fbd87
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 12:08:37 +0200

    Student Service

--- Software_Code/Database/Service/StudentService.java
+++ Software_Code/Database/Service/StudentService.java
@@ -1,4 +1,18 @@
 package Software_Code.Database.Service;
 
+import Software_Code.Database.Dao.StudentDao;
+import Software_Code.Database.Model.Student;
+import java.util.List;
+
 public class StudentService {
+    private final StudentDao studentDao = new StudentDao();
+
+    public void registerStudent(int student_id, String name, String major, String email) {
+        Student student = new Student(student_id, name, major, email);
+        studentDao.addStudent(student);
+    }
+
+    public List<Student> listAllStudents() {
+        return studentDao.getAllStudents();
+    }
 }
