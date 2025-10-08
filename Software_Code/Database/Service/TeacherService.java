07236e9b6f4a2acba84719c6f49e86887ed86522
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 12:08:44 +0200

    Teacher Service

--- Software_Code/Database/Service/TeacherService.java
+++ Software_Code/Database/Service/TeacherService.java
@@ -1,4 +1,18 @@
 package Software_Code.Database.Service;
 
+import Software_Code.Database.Dao.TeacherDao;
+import Software_Code.Database.Model.Teacher;
+import java.util.List;
+
 public class TeacherService {
+    private final TeacherDao teacherDao = new TeacherDao();
+
+    public void registerTeacher(int teacher_id, String name, String email) {
+        Teacher teacher = new Teacher(teacher_id, name, email);
+        teacherDao.addTeacher(teacher);
+    }
+
+    public List<Teacher> listAllTeachers() {
+        return teacherDao.getAllTeachers();
+    }
 }
