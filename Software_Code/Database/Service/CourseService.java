d4d703d5371935317b3a172eaa423a402390bbea
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 12:08:30 +0200

    Course Service

--- Software_Code/Database/Service/CourseService.java
+++ Software_Code/Database/Service/CourseService.java
@@ -1,4 +1,18 @@
 package Software_Code.Database.Service;
 
+import Software_Code.Database.Dao.CourseDao;
+import Software_Code.Database.Model.Course;
+import java.util.List;
+
 public class CourseService {
+    private final CourseDao courseDao = new CourseDao();
+
+    public void registerCourse(int course_id, int teacher_id, int student_id) {
+        Course course = new Course(course_id, teacher_id, student_id);
+        courseDao.addCourse(course);
+    }
+
+    public List<Course> listAllCourses() {
+        return courseDao.getAllCourses();
+    }
 }
