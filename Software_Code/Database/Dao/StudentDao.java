96fef2576d5984a2dce7c665020e2d6e23822835
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 12:00:06 +0200

    StudentDao

--- Software_Code/Database/Dao/StudentDao.java
+++ Software_Code/Database/Dao/StudentDao.java
@@ -1,4 +1,51 @@
 package Software_Code.Database.Dao;
 
+import Software_Code.Database.Model.Student;
+import Software_Code.Backend_Server.DB_Connection.DB_Connection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentDao {
+    public void addStudent(Student student) {
+        String sql = "INSERT INTO student (student_id, name, major, email) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DB_Connection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, student.getId());
+            stmt.setString(2, student.getName());
+            stmt.setString(3, student.getMajor());
+            stmt.setString(4, student.getName());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        String sql = "SELECT * FROM student";
+        List<Student> students = new ArrayList<>();
+
+        try (Connection conn = DB_Connection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                students.add(new Student(
+                        rs.getInt("student_id"),
+                        rs.getString("name"),
+                        rs.getString("major"),
+                        rs.getString("email")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return students;
+    }
 }
