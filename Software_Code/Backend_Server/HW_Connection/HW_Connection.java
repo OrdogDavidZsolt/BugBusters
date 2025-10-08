873e700b3e8f2ac76eedc36d24f3465f87c35ff4
TBence04 <t.bence.unideb@gmail.com>
2025-10-08 16:55:40 +0200

    On branch main  Changes to be committed: 	modified:   Software_Code/Backend_Server/HW_Connection/HW_Connection.java
    git fix for me

--- Software_Code/Backend_Server/HW_Connection/HW_Connection.java
+++ Software_Code/Backend_Server/HW_Connection/HW_Connection.java
@@ -15,27 +15,28 @@ public class HW_Connection
     private static final int THREAD_POOL_SIZE = 50; // egyszerre max. 50 kliens
 
     public static void start_HW_Server() {
-
     
-        ExecutorService pool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-        //^ ez egy szál kezelő szolgáltatás, létrehoz egy olyan szál poolt, ami legfeljebb 50 klien kezelését engedi
-
-        try(ServerSocket serverSocket = new ServerSocket(PORT))
+        //külön listener szál az accept() blokkoló tulajdonsága miatt --> így most megy a program a fő szálon, minden más mehet mellette a szervernél.
+        // Ha jön egy kliens, akkor létrejön egy külön szál, ami megszűnik, ha elvégzi a feladatát.
+        new Thread(() ->
         {
-            System.out.println(">>HW_Connection: HW Szerver elindult a " + PORT + " porton...");
+            ExecutorService pool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+            //^ ez egy szál kezelő szolgáltatás, létrehoz egy olyan szál poolt, ami legfeljebb 50 kliens kezelését engedi
 
-            while (true) // a szerver folyamatosan figyeli a klienseket
-            {
-                Socket clientSocket = serverSocket.accept(); //ez a blokkoló hívás, vagyis addig várunk, amíg egy kliens csatlakozik
-                //^ ha jön egy új kliens, akkor létrejön egy Socket objektum, ami az adott klienssel kommunikál
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                System.out.println(">>HW_Connection: HW Szerver elindult a " + PORT + " porton...");
 
-                pool.execute(new ClientHandler(clientSocket));
+                while (true) // a szerver folyamatosan figyeli a klienseket
+                {
+                    Socket clientSocket = serverSocket.accept(); //ez a blokkoló hívás, vagyis addig várunk, amíg egy kliens csatlakozik
+                    //^ ha jön egy új kliens, akkor létrejön egy Socket objektum, ami az adott klienssel kommunikál
+
+                    pool.execute(new ClientHandler(clientSocket));
+                }
+            } catch (IOException e) {
+                System.out.println(">>HW_Connection: IOException: " + e.getMessage());  // ha hiba van kiirjuk mi a baj
             }
-        }
-        catch (IOException e)
-        {
-            System.out.println(">>HW_Connection: IOException: " + e.getMessage());  // ha hiba van kiirjuk mi a baj
-        }
+        }).start();
     }
 
     public static int getPort()
