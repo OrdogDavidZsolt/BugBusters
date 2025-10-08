bfbedc2efb1ade2221144818c409d1b1d2ebe8ae
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 11:04:14 +0200

    Fontos kiegészítés a kapcsolatot illetően

--- Software_Code/Backend_Server/DB_Connection/DB_Connection.java
+++ Software_Code/Backend_Server/DB_Connection/DB_Connection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB_Connection {
-    private static final String URL = "jdbc:mysql://localhost:3306/users"; //online adatbázis linkje és belépési infók átírása
+    private static final String URL = "jdbc:mysql://localhost:3306/example"; //online adatbázis linkje és belépési infók átírása
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
