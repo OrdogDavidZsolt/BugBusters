99eb719613cd47a1a27011639f6ef88ac145da28
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-08 10:43:55 +0200

    DB Connection és hozzá szükséges connector fájl

--- Software_Code/Backend_Server/DB_Connection/DB_Connection.java
+++ Software_Code/Backend_Server/DB_Connection/DB_Connection.java
@@ -1,5 +1,15 @@
 package Software_Code.Backend_Server.DB_Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB_Connection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/users"; //online adatbázis linkje és belépési infók átírása
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
