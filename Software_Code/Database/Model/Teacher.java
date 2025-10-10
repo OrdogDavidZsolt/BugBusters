011aba91a3bba91b2a1ac14f0db58a26bbe7e5eb
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-10 16:42:45 +0200

    Teacher model changes

--- Software_Code/Database/Model/Teacher.java
+++ Software_Code/Database/Model/Teacher.java
@@ -1,40 +1,37 @@
 package Software_Code.Database.Model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teacher")
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teacher_id;
+
     private String name;
     private String email;
 
-    //Constructor for retrieving from DB (with id)
+    public Teacher() {}
+
     public Teacher(int id, String name, String email) {
         this.teacher_id = id;
         this.name = name;
         this.email = email;
     }
 
-    public int getId() {
-        return teacher_id;
-    }
+    public int getId() { return teacher_id; }
 
-    public void setId(int id) {
-        this.teacher_id = id;
-    }
+    public void setId(int id) { this.teacher_id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
