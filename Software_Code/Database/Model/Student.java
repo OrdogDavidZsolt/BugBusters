3dcdbaaeed5d7169ac819c0841601519f12bc90a
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-10 16:42:38 +0200

    Student model changes

--- Software_Code/Database/Model/Student.java
+++ Software_Code/Database/Model/Student.java
@@ -1,10 +1,19 @@
 package Software_Code.Database.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int student_id;
+
     private String name;
     private String major;
     private String email;
+
+    public Student() {}
 
     //Constructor for retrieving from DB (with id)
     public Student(int id, String name, String major, String email) {
@@ -14,37 +23,21 @@ public class Student {
         this.email = email;
     }
 
-    public int getId() {
-        return student_id;
-    }
+    public int getId() { return student_id; }
 
-    public void setId(int id) {
-        this.student_id = id;
-    }
+    public void setId(int id) { this.student_id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getMajor() {
-        return major;
-    }
+    public String getMajor() { return major; }
 
-    public void setMajor(String major) {
-        this.major = major;
-    }
+    public void setMajor(String major) { this.major = major; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
