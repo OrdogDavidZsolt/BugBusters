4bd38dc39fad929478b2baf397daff10ee3ffa49
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-10-10 16:42:31 +0200

    Course model changes

--- Software_Code/Database/Model/Course.java
+++ Software_Code/Database/Model/Course.java
@@ -1,9 +1,22 @@
 package Software_Code.Database.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int course_id;
+
     private int teacher_id;
     private int student_id;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    public Course() {}
 
     public Course(int course_id, int teacher_id, int student_id) {
         this.course_id = course_id;
@@ -11,29 +24,17 @@ public class Course {
         this.student_id = student_id;
     }
 
-    public int getCourse_id() {
-        return course_id;
-    }
+    public int getCourse_id() { return course_id; }
 
-    public void setCourse_id(int course_id) {
-        this.course_id = course_id;
-    }
+    public void setCourse_id(int course_id) { this.course_id = course_id; }
 
-    public int getTeacher_id() {
-        return teacher_id;
-    }
+    public int getTeacher_id() { return teacher_id; }
 
-    public void setTeacher_id(int teacher_id) {
-        this.teacher_id = teacher_id;
-    }
+    public void setTeacher_id(int teacher_id) { this.teacher_id = teacher_id; }
 
-    public int getStudent_id() {
-        return student_id;
-    }
+    public int getStudent_id() { return student_id; }
 
-    public void setStudent_id(int student_id) {
-        this.student_id = student_id;
-    }
+    public void setStudent_id(int student_id) { this.student_id = student_id; }
 
     @Override
     public String toString() {
