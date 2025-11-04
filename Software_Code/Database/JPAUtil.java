a8a6ef1f1daf0ea0612a3851dd68e4e40eb2664f
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-11-04 15:19:23 +0100

    JPAUtil

---
+++ Software_Code/Database/JPAUtil.java
@@ -0,0 +1,15 @@
+package Software_Code.Database;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("br.com.fredericci.pu");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
+
