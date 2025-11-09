91a5c583053da8c662c9c258f9b20d8bc2c7eb3e
greti <szvgrt2004@mailbox.unideb.hu>
2025-11-09 20:08:39 +0100

    ip felhasználásának módósítása a sencCommandToReader metódusban 1

--- Software_Code/Backend_Server/HW_Connection/HW_Connection.java
+++ Software_Code/Backend_Server/HW_Connection/HW_Connection.java
@@ -191,7 +191,7 @@ public class HW_Connection
 // hasznalat: HW_Connection.sendCommandToReader("DEV-003", HW_Command.RED_LED_ON);
 public static void sendCommandToReader(String readerId, HW_Command command)
 {
-    String ip = readers.get(readerId);
+    String targtetip = readers.entrySet().stream()
 
     if (ip == null)
     {
